d4600bf1c63983d944f12d1d56c65bd8abf96767
Fern233 <47768840+Fern233@users.noreply.github.com>
2019-03-03 22:54:04 +1100

    Add files via upload

---
+++ count.py
@@ -0,0 +1,25 @@
+
+
+def count_char(text):
+    seen = set()
+for x in text
+        if(x not in seen){
+        print x + text.count(x)\n
+        }
+        seen.add(x)
+    pass
+
+def count_char_insensitive(text):
+    # TODO do the same as `count_char` but in a case-insensitive manner
+    pass
+
+
+def count_char_ordered(text):
+
+    def ct(text,i):
+        c = text.count[i]
+        return c
+    for x in text
+        
+        print sorted(text,key = ct)
+    pass
---
+++ number_list.py
@@ -0,0 +1,38 @@
+
+def find_reverse(numbers):
+    numbers.reverse()
+    pass
+
+def find_max(numbers):
+    max_n = max(numbers)
+    pass
+
+def find_min(numbers):
+    min_n = min(numbers)
+    pass
+
+def find_sum(numbers):
+    sum_n = sum(numbers)
+    pass
+
+def find_average(numbers)
+    aver_n = sum(numbers)/len(numbers)
+    pass
+
+def find_descending(numbers):
+    numbers.sort(reverse = True)
+    pass
+
+def second_smallest(numbers):
+    numbers.sort()
+    min_2 = numbers[1]
+    pass
+
+def kth_smallest(numbers, k):
+    numbers.sort()
+    min_2 = numbers[k-1]
+    pass
+
+if __name__ == '__main__':
+   print 'it is running'
+    pass
